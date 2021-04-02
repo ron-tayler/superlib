beb68a2bc2265d93d07bf79e018b2ff59a0acb89
Ron Tayler <61260112+ron-tayler@users.noreply.github.com>
2021-04-03 01:48:26 +0300

    Update lib_functions.php
    Были замечены непонятные символы вызывающие ошибку синтаксиса. Заменил на пробелы.

--- lib_functions.php
+++ lib_functions.php
@@ -2,7 +2,7 @@
 
 function get_ip()
 {
-    if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+    if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
         return $_SERVER['REMOTE_ADDR'];
     } else {
         // for those, who gets "Undefined index: REMOTE_ADDR" error because of console run
